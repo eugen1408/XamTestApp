ff542d3d00fab635c9e804c7f7ce0382667894c9
evaganov <evaganov@outlook.com>
2020-08-17 22:58:05 +0500

    Create Consts.cs

---
+++ XamTestApp/XamTestApp/Consts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using SQLite;
+
+namespace XamTestApp
+{
+    public static class Consts
+    {
+
+        #region SQLite
+        public const string DatabaseFilename = "ContactsSQLite.db3";
+        public const SQLiteOpenFlags DatabaseFlags = SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.SharedCache;
+
+        public static string DatabasePath
+        {
+            get
+            {
+                var basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(basePath, DatabaseFilename);
+            }
+        }
+
+        #endregion
+
+        public const int CacheTTLMinutes = 1;
+    }
+}
