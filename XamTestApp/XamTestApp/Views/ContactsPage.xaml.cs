54957313eb58817fcf022164adf9e45baa0055a5
evaganov <evaganov@outlook.com>
2020-08-17 22:56:54 +0500

    Add ContactsPage

---
+++ XamTestApp/XamTestApp/Views/ContactsPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using XamTestApp.ViewModels;
+
+namespace XamTestApp.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class ContactsPage : ContentPage
+    {
+        private readonly ContactsViewModel _viewModel;
+
+        public ContactsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ContactsViewModel();
+        }
+
+        private async void OnItemSelected(object sender, EventArgs args)
+        {
+            var layout = (BindableObject)sender;
+            var item = (ContactViewModel)layout.BindingContext;
+            await Navigation.PushAsync(new ContactDetailsPage(item));
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (_viewModel.Contacts.Count == 0)
+            {
+                _viewModel.LoadContactsCommand.Execute(null);
+            }
+        }
+    }
+}
