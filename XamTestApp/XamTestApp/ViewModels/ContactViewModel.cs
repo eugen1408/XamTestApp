861e23c63fe4b3187077e8b16a5f9dddf66d0c69
evaganov <evaganov@outlook.com>
2020-08-17 22:55:27 +0500

    Add ViewModels

--- XamTestApp/XamTestApp/ViewModels/ContactViewModel.cs
+++ XamTestApp/XamTestApp/ViewModels/ContactViewModel.cs
@@ -1,6 +1,70 @@
-﻿namespace XamTestApp.ViewModels
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using XamTestApp.Views;
+using XamTestAppDataLibrary;
+using XamTestAppDataLibrary.Models;
+
+namespace XamTestApp.ViewModels
 {
-    internal class ContactViewModel : BaseViewModel
+    public class ContactViewModel : BaseViewModel
     {
+        #region Fields & Properties
+        private readonly Contact _contact;
+        private readonly Lazy<string> _phoneDigits;
+        public Command OpenContactDetailsCommand { get; set; }
+        public Command OpenPhoneDialerCommand { get; set; }
+
+        public string PhoneDigits => _phoneDigits.Value;
+        public string Name => _contact.Name;
+        public string Phone => _contact.Phone;
+        public float Height => _contact.Height;
+        public Temperament Temperament => _contact.Temperament;
+
+
+        // в некоторых объектах Start > End, вопрос, насколько это корректно и надо ли как-то обрабатывать в рамках теста
+        public string EducationPeriod => _contact.EducationPeriod is null ? "Период не указан" :
+            $"{_contact.EducationPeriod.Start.DateTime.ToShortDateString()} - {_contact.EducationPeriod.End.DateTime.ToShortDateString()}";
+
+        public string Biography => _contact.Biography; 
+        #endregion
+        public ContactViewModel(Contact contact)
+        {
+            _contact = contact ?? throw new ArgumentNullException(nameof(contact));
+            OpenContactDetailsCommand = new Command(async () => await ExecuteOpenContactDetailsCommand());
+            OpenPhoneDialerCommand = new Command(async () => await ExecuteOpenPhoneDialerCommand());
+            _phoneDigits = new Lazy<string>(GetPhoneDigits);
+        }
+
+        private string GetPhoneDigits()
+        {
+            var phoneArray = _contact.Phone.ToCharArray();
+            phoneArray = Array.FindAll(phoneArray, (p => char.IsDigit(p)));
+            var phoneDigits = new string(phoneArray);
+            return phoneDigits;
+        }
+
+        #region Commands
+
+        private async Task ExecuteOpenContactDetailsCommand() =>
+            await Shell.Current.Navigation.PushAsync(new ContactDetailsPage(this));
+
+        private async Task ExecuteOpenPhoneDialerCommand()
+        {
+            try
+            {
+                PhoneDialer.Open(Phone);
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                Debug.WriteLine(ex);
+#endif
+                await Shell.Current.DisplayAlert("Ошибка", $"Ошибка при поптыке набора номера {Phone}", "ОК");
+            }
+        } 
+        #endregion
     }
 }
---
+++ XamTestApp/XamTestApp/ViewModels/ContactsViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamTestApp.Services;
+
+namespace XamTestApp.ViewModels
+{
+    public class ContactsViewModel : BaseViewModel
+    {
+        #region Fields & Properties
+        public ObservableCollection<ContactViewModel> Contacts { get; set; }
+        public Command LoadContactsCommand { get; set; }
+        public Command ForceLoadContactsCommand { get; set; } 
+        #endregion
+
+        public ContactsViewModel()
+        {
+            Contacts = new ObservableCollection<ContactViewModel>();
+            LoadContactsCommand = new Command(async () => await ExecuteLoadContactsCommand());
+            ForceLoadContactsCommand = new Command(async () => await ExecuteLoadContactsCommand(LoadContactsMethod.ForceReload));
+        }
+
+        #region Commands
+        private async Task ExecuteLoadContactsCommand(LoadContactsMethod loadContactsMethod = LoadContactsMethod.Default)
+        {
+            IsBusy = true;
+            try
+            {
+                var contacts = await ContactsDataStore.GetContactsAsync(loadContactsMethod);
+                Contacts.Clear();
+                foreach (var contact in contacts)
+                {
+                    Contacts.Add(new ContactViewModel(contact));
+                }
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                Debug.WriteLine(ex);
+#endif
+                await Shell.Current.DisplayAlert($"Ошибка", "Произошла ошибка при загрузке контактов. Будут показаны контакты из локальной БД.", "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+            // если это первая загрузка и список пустой, загрузим из кэша
+            if (Contacts.Count == 0)
+            {
+                await LoadContactsFromCache();
+            }
+        }
+
+        private async Task LoadContactsFromCache()
+        {
+            IsBusy = true;
+            try
+            {
+                var contacts = await ContactsDataStore.GetContactsAsync(LoadContactsMethod.ReloadFromCache);
+                foreach (var contact in contacts)
+                {
+                    Contacts.Add(new ContactViewModel(contact));
+                }
+            }
+            catch (Exception ex)
+            {
+#if DEBUG
+                Debug.WriteLine(ex);
+#endif
+                await Shell.Current.DisplayAlert($"Ошибка", "Произошла ошибка при загрузке контактов из локальной БД.", "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+        #endregion
+    }
+}
