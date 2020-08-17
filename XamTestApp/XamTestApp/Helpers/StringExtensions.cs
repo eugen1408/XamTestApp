89ea51872a3b6539eec4e115dd5e8a1101f56652
evaganov <evaganov@outlook.com>
2020-08-17 22:54:56 +0500

    Add Search string extensions

---
+++ XamTestApp/XamTestApp/Helpers/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace XamTestApp.Helpers
+{
+    internal static class StringExtensions
+    {
+        public static bool FoundInAny(this string search, params string[] searchSource)
+        {
+            if (searchSource == null || string.IsNullOrEmpty(search))
+            {
+                return false;
+            }
+
+            var searchCharArray = search.ToCharArray();
+
+            searchCharArray = Array.FindAll(searchCharArray, (p => char.IsLetterOrDigit(p) || char.IsWhiteSpace(p)));
+            var searchNormalized = new string(searchCharArray);
+
+            var str = string.Join(" ", searchSource);
+            return searchNormalized.Split(' ').All(a => str.IndexOf(a, StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+    }
+}
