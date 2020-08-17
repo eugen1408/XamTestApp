c21cc451a8c223160257c74ed492ddda6694aac9
evaganov <evaganov@outlook.com>
2020-08-17 22:57:02 +0500

    Add ContactDetailsPage

---
+++ XamTestApp/XamTestApp/Views/ContactDetailsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamTestApp.ViewModels;
+
+namespace XamTestApp.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class ContactDetailsPage : ContentPage
+    {
+        private readonly ContactViewModel _viewModel;
+
+        public ContactDetailsPage(ContactViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = _viewModel = viewModel;
+        }
+    }
+}
