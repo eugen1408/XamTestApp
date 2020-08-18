7998d3ec43d227e0316db6d4c125a86cdda15ff9
evaganov <evaganov@outlook.com>
2020-08-18 12:50:56 +0500

    Add XamTestApp.Tests

---
+++ Tests/XamTestApp.Tests/ContactViewModelTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XamTestApp.ViewModels;
+using XamTestAppDataLibrary.Models;
+using Xunit;
+
+namespace XamTestApp.Tests
+{
+    public class ContactViewModelTests
+    {
+        [Theory]
+        [InlineData("7 922 222 10 82", "79222221082")]
+        [InlineData("7 (922) 222      10-82", "79222221082")]
+        [InlineData("+7 922 222-10-82", "79222221082")]
+        [InlineData("rhgrewh 7 922 222 10 82 afedafa", "79222221082")]
+        public void PhoneDigits_PhoneStrings_ShouldFormatPhone(string phone, string expected)
+        {
+            // Arrange
+            var contact = new Contact()
+            {
+                Phone = phone
+            };
+            var contactViewModel = new ContactViewModel(contact);
+
+            // Act
+            var actual = contactViewModel.PhoneDigits;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Tests/XamTestApp.Tests/StringExtensionsTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XamTestApp.Helpers;
+using Xunit;
+
+namespace XamTestApp.Tests
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData("tEsT", "fjiefjeifTESTewgjg")]
+        [InlineData("test", "пкпкп", "eftestelab")]
+        [InlineData("test test2", "пкпеуыекп", "eftest2lab")]
+        [InlineData("test aa5", "пкпеуыекп", "wgTESTjr", null, "wjkggaa5")]
+        public void FoundInAny_SimpleStrings_ShouldSearchSubstringsCaseInsensitive(string searchString, params string[] arrayToSearch)
+        {
+            // Arrange
+
+
+            // Act
+
+            var actual = searchString.FoundInAny(arrayToSearch);
+
+            // Assert
+
+            Assert.True(actual);
+        }
+
+        [Theory]
+        [InlineData("7 (922 222 1082", "79222221082")]
+        [InlineData("922222", "пкпкп", "79222221082")]
+        [InlineData("test 792-2-2-2-2-(1)0 8 2", "TEST", "79222221082")]
+        [InlineData("7(922)222-1082", "test", "", "   ", null, "79222221082")]
+        public void FoundInAny_PhoneDigitStrings_ShouldSearchSubstringsAnyFormat(string searchString, params string[] arrayToSearch)
+        {
+            // Arrange
+
+
+            // Act
+
+            var actual = searchString.FoundInAny(arrayToSearch);
+
+            // Assert
+
+            Assert.True(actual);
+        }
+    }
+}
